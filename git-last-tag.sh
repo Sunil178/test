c0acb864265aaecfbd26b7ebbd35c49811968870
Sunil Thakur <sunilthakur178co@gmail.com>
2022-05-06 21:33:53 +0530

    Making of multiple HEAD build strings

---
+++ git-last-tag.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+get_git_build_number () {
+	CURRENT_TAG=`git tag --points-at HEAD`
+	BUILD='build'
+	if [[ "$CURRENT_TAG" == *"$BUILD"* ]]; then
+		BUILD_REPS="${CURRENT_TAG//[^build]}"
+		BUILD_COUNT=${#BUILD_REPS}
+		if [ $BUILD_COUNT -gt 5 ]; then
+			echo "error: More than one build tags are found"
+			exit
+		fi
+		VALUE=${CURRENT_TAG#*-}
+		local -n VAR=$1
+		VAR=$VALUE
+	else
+		TAGS=`git tag`
+		SAVEIFS=$IFS
+		IFS=$'\n'
+		TAGS=($TAGS)
+		IFS=$SAVEIFS
+		MAX=0
+		for (( i=0; i<${#TAGS[@]}; i++ ))
+		do
+			if [[ "${TAGS[$i]}" == *"$BUILD"* ]]; then
+				VALUE=${TAGS[$i]#*-}
+				(($VALUE > MAX)) && MAX=$VALUE
+			fi
+		done
+		local -n VAR=$1
+		VAR=$(($MAX + 1))
+	fi
+}
+
+get_git_build_number TAG
+echo "$TAG"
+
